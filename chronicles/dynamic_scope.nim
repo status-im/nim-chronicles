c154ff6cfd2e3473dacd4bab928ec1c26d79b811
Zahary Karadjov <zahary@gmail.com>
2018-04-26 01:34:34 +0300

    store the dynamic bindings on the stack (to save memory allocations)

--- chronicles/dynamic_scope.nim
+++ chronicles/dynamic_scope.nim
@@ -2,33 +2,50 @@ import
   macros, log_output, scope_helpers
 
 type
-  ScopeBindingBase = ref object of RootObj
+  ScopeBindingBase = object of RootObj
     name: string
     appender: LogAppender
 
   LogAppender = proc(x: var LogOutput, valueAddr: ScopeBindingBase)
 
-  ScopeBinding[T] = ref object of ScopeBindingBase
+  ScopeBinding[T] = object of ScopeBindingBase
     value: T
+
+  BindingsArray = ptr UncheckedArray[ptr ScopeBindingBase]
+
+  BindingsFrame = object
+    prev: ptr BindingsFrame
+    bindings: BindingsArray
+    bindingsCount: int
 
 proc appenderIMPL[T](log: var LogOutput, valueAddr: ScopeBindingBase) =
   let v = ScopeBinding[T](valueAddr)
   log.setProperty v.name, v.value
 
-var dynamicProperties {.threadvar.}: seq[ScopeBindingBase]
+var topBindingFrame {.threadvar.}: ptr BindingsFrame
 
 proc chroniclesThreadInit* =
   # This must be called in each thread that is going to use logging
-  newSeq(dynamicProperties, 0)
+  topBindingFrame = nil
 
-proc addDynamicProp[T](name: string, value: T) =
-  dynamicProperties.add ScopeBinding[T](name: name,
-                                        value: value,
-                                        appender: appenderIMPL[T])
+proc makeScopeBinding[T](name: string, value: T): ScopeBinding[T] =
+  result.name = name
+  result.appender = appenderIMPL[T]
+  result.value = value
 
 proc logAllDynamicProperties*(log: var LogOutput) =
   # This proc is intended for internal use only
-  for p in dynamicProperties: p.appender(log, p)
+  var frame = topBindingFrame
+  while frame != nil:
+    for i in 0 ..< frame.bindingsCount:
+      let binding = frame.bindings[i]
+      binding.appender(log, binding[])
+    frame = frame.prev
+
+template makeBindingsFrame(bindings: array): auto =
+  BindingsFrame(prev: topBindingFrame,
+                bindings: cast[BindingsArray](unsafeAddr bindings),
+                bindingsCount: bindings.len)
 
 macro dynamicLogScope*(args: varargs[untyped]): untyped =
   # XXX: open question: should we support overriding
@@ -37,22 +54,37 @@ macro dynamicLogScope*(args: varargs[untyped]): untyped =
   let body = args[^1]
   args.del(args.len - 1)
 
+  let
+    makeScopeBinding = bindSym"makeScopeBinding"
+
   if body.kind != nnkStmtList:
     error "dynamicLogScope expects a block"
 
-  let addDynamicProp = bindSym"addDynamicProp"
+  var
+    bindingsVars = newTree(nnkStmtList)
+    bindingsArray = newTree(nnkBracket)
+    bindingsArraySym = genSym(nskLet, "bindings")
 
-  var setProps = newTree(nnkStmtList)
   for name, value in assignments(args):
-    setProps.add newCall(addDynamicProp, newLit(name), value)
+    var bindingVar = genSym(nskLet, name)
+
+    bindingsVars.add quote do:
+      let `bindingVar` = `makeScopeBinding`(`name`, `value`)
+
+    bindingsArray.add newCall(newIdentNode("unsafeAddr"), bindingVar)
+
+  let totalBindingVars = bindingsVars.len
 
   result = quote:
-    var currentDynamicScopesEntries = dynamicProperties.len
+    let prevBindingFrame = topBindingFrame
     try:
-      `setProps`
+      `bindingsVars`
+      let `bindingsArraySym` = `bindingsArray`
+      let bindingFrame = makeBindingsFrame(`bindingsArraySym`)
+      topBindingFrame = unsafeAddr bindingFrame
       `body`
     finally:
-      dynamicProperties.setLen currentDynamicScopesEntries
+      topBindingFrame = prevBindingFrame
 
 chroniclesThreadInit()
 
