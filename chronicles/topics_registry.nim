d14e130218d2e65487dd6a647aa4aafc11e21b00
Ștefan Talpalaru <stefantalpalaru@yahoo.com>
2019-03-20 01:28:06 +0100

    topics registry: globals, locks and gcsafe blocks

--- chronicles/topics_registry.nim
+++ chronicles/topics_registry.nim
@@ -1,4 +1,4 @@
-import macros, tables
+import locks, macros, tables
 from options import LogLevel
 
 type
@@ -15,94 +15,112 @@ type
   TopicsRegistry* = object
     totalEnabledTopics*: int
     totalRequiredTopics*: int
-    topicStatesTable*: TableRef[string, Topic]
+    topicStatesTable*: Table[string, Topic]
 
-var gActiveLogLevel: LogLevel
+template lock(someLock: Lock, body: untyped) =
+  {.locks: [someLock].}:
+    someLock.acquire()
+    try:
+      body
+    finally:
+      someLock.release()
+
+var
+  gActiveLogLevelLock: Lock
+  gActiveLogLevel {.guard: gActiveLogLevelLock.}: LogLevel
+initLock(gActiveLogLevelLock)
+
+template lockGActiveLogLevel(body: untyped) =
+  lock(gActiveLogLevelLock, body)
 
 proc setLogLevel*(lvl: LogLevel) =
-  gActiveLogLevel = lvl
+  lockGActiveLogLevel:
+    gActiveLogLevel = lvl
 
-var topicsRegistry {.threadvar.}: ref TopicsRegistry
+var
+  registryLock: Lock
+  registry {.guard: registryLock.}: TopicsRegistry
+initLock(registryLock)
+registry.topicStatesTable = initTable[string, Topic]()
 
-proc getTopicsRegistry*(): ref TopicsRegistry =
-  # "topicsRegistry" needs to be initialised in each thread.
-  # Since we don't know which procedures are going to deal with an uninitialised
-  # threadvar, we're having them all access it through this getter.
-  if topicsRegistry.isNil():
-    topicsRegistry = new TopicsRegistry
-  if topicsRegistry.topicStatesTable.isNil():
-    topicsRegistry.topicStatesTable = newTable[string, Topic]()
-  return topicsRegistry
+# don't `return` from the body, or the lock won't be released
+template lockRegistry(body: untyped) =
+  {.gcsafe.}: # this will, of course, break if https://github.com/nim-lang/RFCs/issues/142 is implemented
+    lock(registryLock, body)
 
 proc clearTopicsRegistry* =
-  var registry = getTopicsRegistry()
-  registry.totalEnabledTopics = 0
-  registry.totalRequiredTopics = 0
-  for val in registry.topicStatesTable.mvalues():
-    val.state = Normal
-
-iterator topicStates*: (string, TopicState) =
-  var registry = getTopicsRegistry()
-  for name, topic in registry.topicStatesTable:
-    yield (name, topic.state)
+  lockRegistry:
+    registry.totalEnabledTopics = 0
+    registry.totalRequiredTopics = 0
+    registry.topicStatesTable.clear()
 
 proc registerTopic*(name: string, topic: Topic) =
-  var registry = getTopicsRegistry()
-  registry.topicStatesTable[name] = topic
+  lockRegistry:
+    registry.topicStatesTable[name] = topic
 
-proc setTopicState*(name: string,
-                    newState: TopicState,
-                    logLevel = LogLevel.NONE): bool =
-  var registry = getTopicsRegistry()
-  if not registry.topicStatesTable.hasKey(name):
-    return false
+proc setTopicState*(name: string, newState: TopicState, logLevel = LogLevel.NONE): bool =
+  result = true
+  lockRegistry:
+    block registryBlock: # for early exits without bypassing the lock release
+      if not registry.topicStatesTable.hasKey(name):
+        result = false
+        break
 
-  var topic = registry.topicStatesTable[name]
+      var topic = registry.topicStatesTable[name]
 
-  case topic.state
-  of Enabled: dec registry.totalEnabledTopics
-  of Required: dec registry.totalRequiredTopics
-  else: discard
-
-  case newState
-  of Enabled: inc registry.totalEnabledTopics
-  of Required: inc registry.totalRequiredTopics
-  else: discard
-
-  topic.state = newState
-  topic.logLevel = logLevel
-  registry.topicStatesTable[name] = topic
-
-  return true
-
-proc topicsMatch*(logStmtLevel: LogLevel,
-                  logStmtTopics: openarray[Topic]): bool =
-  var
-    registry = getTopicsRegistry()
-    hasEnabledTopics = registry.totalEnabledTopics > 0
-    enabledTopicsMatch = false
-    normalTopicsMatch = logStmtTopics.len == 0 and logStmtLevel >= gActiveLogLevel
-    requiredTopicsCount = registry.totalRequiredTopics
-
-  for topic in logStmtTopics:
-    let topicLogLevel = if topic.logLevel != NONE: topic.logLevel
-                        else: gActiveLogLevel
-    if logStmtLevel >= topicLogLevel:
       case topic.state
-      of Normal: normalTopicsMatch = true
-      of Enabled: enabledTopicsMatch = true
-      of Disabled: return false
-      of Required: dec requiredTopicsCount
+      of Enabled: dec registry.totalEnabledTopics
+      of Required: dec registry.totalRequiredTopics
+      else: discard
 
-  if requiredTopicsCount > 0:
-    return false
+      case newState
+      of Enabled: inc registry.totalEnabledTopics
+      of Required: inc registry.totalRequiredTopics
+      else: discard
 
-  if hasEnabledTopics and not enabledTopicsMatch:
-    return false
+      topic.state = newState
+      topic.logLevel = logLevel
+      registry.topicStatesTable[name] = topic
 
-  return normalTopicsMatch
+proc topicsMatch*(logStmtLevel: LogLevel, logStmtTopics: openarray[Topic]): bool =
+  lockRegistry:
+    block registryBlock:
+      var
+        hasEnabledTopics = registry.totalEnabledTopics > 0
+        enabledTopicsMatch = false
+        normalTopicsMatch: bool
+        requiredTopicsCount = registry.totalRequiredTopics
+
+      lockGActiveLogLevel:
+        normalTopicsMatch = logStmtTopics.len == 0 and logStmtLevel >= gActiveLogLevel
+
+      for topic in logStmtTopics:
+        var topicLogLevel: LogLevel
+        lockGActiveLogLevel:
+          topicLogLevel = if topic.logLevel != NONE: topic.logLevel else: gActiveLogLevel
+        if logStmtLevel >= topicLogLevel:
+          case topic.state
+          of Normal:
+            normalTopicsMatch = true
+          of Enabled:
+            enabledTopicsMatch = true
+          of Disabled:
+            result = false
+            break
+          of Required:
+            dec requiredTopicsCount
+
+      if requiredTopicsCount > 0:
+        result = false
+        break
+
+      if hasEnabledTopics and not enabledTopicsMatch:
+        result = false
+        break
+
+      result = normalTopicsMatch
 
 proc getTopicState*(topicName: string): Topic =
-  var registry = getTopicsRegistry()
-  return registry.topicStatesTable[topicName]
+  lockRegistry:
+    result = registry.topicStatesTable[topicName]
 
